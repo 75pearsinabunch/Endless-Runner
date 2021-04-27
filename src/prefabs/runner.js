c707e31c598d62ea5479554e56ca513c41bb135d
“Adrian <adrmille@ucsc.edu>
2021-04-26 18:04:07 -0700

    altered runner class to be a physics object

--- src/prefabs/runner.js
+++ src/prefabs/runner.js
@@ -1,4 +1,4 @@
-class Runner extends Phaser.GameObjects.Sprite {
+class Runner extends Phaser.Physics.Arcade.Sprite {
     constructor(scene, x, y, texture, frame) {
         super(scene, x, y, texture, frame);
 
