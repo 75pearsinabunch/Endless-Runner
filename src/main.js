f63e6e5bc98fe7e4e1024066389428da9c7c387e
75pearsinabunch <75pearsinabunch@gmail.com>
2021-04-30 13:33:21 -0700

    change start position

--- src/main.js
+++ src/main.js
@@ -24,7 +24,7 @@ let gameOptions = {
     platformVerticalLimit: [0.1, 0.8],
     //runnerGravity: 900,
     jumpForce: 400,
-    runnerStartPosition: 600,
+    runnerStartPosition: 100,
     jumps: 2,
     
     floorSpeedRange: [300, 300],
